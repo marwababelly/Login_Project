42ddd36b5a54c9de01adca38194614fd3bafd00e
karkotly <mohamed@mytrendingstories.com>
2022-03-04 22:55:13 +0200

    Reformat comments

--- app.js
+++ app.js
@@ -2,15 +2,16 @@ const togglePassword = document.getElementById("togglePassword");
 const password = document.getElementById("password");
 
 togglePassword.addEventListener("click", function() {
-    // toggle the type attribute
+    // TODO: Consider capitalizing the first character of each comment
+    // Toggle the type attribute
     const type = password.getAttribute("type") === "password" ? "text" : "password";
     password.setAttribute("type", type);
 
-    // toggle the icon
+    // Toggle the icon
     this.classList.toggle("bi-eye");
 });
 
-// prevent form submit
+// Prevent form submit
 const form = document.querySelector("form");
 form.addEventListener('submit', function(e) {
     e.preventDefault();
